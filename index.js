ac2e7d26d7f79c6522b6d7bc7d11105382607c00
ALFIXD <99767269+Fiisya@users.noreply.github.com>
2025-03-24 00:34:08 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -3,20 +3,55 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+
+let totalRequests = 0;
+let clients = [];
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+    totalRequests++;
+    sendUpdateToClients();
+    next();
+});
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const routes = ["igdl", "fbdl", "githubstalk", "searchgroups"];
+app.get("/monitor-page", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "monitor", "monitor.html"));
+});
+
+app.get("/monitor", (req, res) => {
+    res.set({
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive",
+    });
+    res.flushHeaders();
+
+    const clientId = Date.now();
+    const newClient = { id: clientId, res };
+    clients.push(newClient);
+
+    res.write(`data: ${JSON.stringify({ totalRequests })}\n\n`);
+
+    req.on("close", () => {
+        clients = clients.filter(client => client.id !== clientId);
+    });
+});
+
+function sendUpdateToClients() {
+    clients.forEach(client => {
+        client.res.write(`data: ${JSON.stringify({ totalRequests })}\n\n`);
+    });
+}
+
+const routes = ["ytdl", "twitterdl", "igdl", "fbdl", "ttdl", "githubstalk", "searchgroups", "ttsearch", "ytsearch", "llama-3.3-70b-versatile", "txt2img", "ssweb", "tahukahkamu"];
 routes.forEach(route => {
     app.use(`/api/${route}`, require(`./api/${route}`));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server berjalan di http://localhost:${PORT}`);
-});
+module.exports = app;
